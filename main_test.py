687ababc76470fa0b238a10516e21e8dd56665a1
fredmloth <frederiekeloth@gmail.com>
2025-09-20 19:56:29 +0200

    added results to gitognore and correctly track time and save plots in main

--- main_test.py
+++ main_test.py
@@ -8,9 +8,9 @@ import Fred_analysis
 
 if __name__ == "__main__":
     runs = 5
-    generations = 30
-    steps = 2000
-    show_plots = False  # Toggle to show plots
+    generations = 50
+    steps = 20000
+    show_plots = True  # Toggle to show plots
 
     tanh_histories = []
     sigmoid_histories = []
@@ -64,17 +64,17 @@ if __name__ == "__main__":
     tanh_histories = np.array(tanh_histories)
     sigmoid_histories = np.array(sigmoid_histories)
 
-    # Run analysis in sigmoid and tanh
+    # Run analysis on sigmoid and tanh
     fig1 = Fred_analysis.plot_individual_runs(tanh_histories, "Tanh")
     fig2 = Fred_analysis.plot_individual_runs(sigmoid_histories, "Sigmoid")
-    fig3 = Fred_analysis.plot_average(tanh_histories, sigmoid_histories)
+    fig3 = Fred_analysis.plot_average_sig_tanh(tanh_histories, sigmoid_histories)
 
     # Save plots
     fig1.savefig("results/tanh_individual_runs.png")
     fig2.savefig("results/sigmoid_individual_runs.png")
     fig3.savefig("results/tanh_vs_sigmoid_average.png")
 
-    # Optionally show plots
+    # Optional: Show plots
     if show_plots:
         import matplotlib.pyplot as plt
         plt.show()

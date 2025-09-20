e6171ee1f88f359f712d27ff9b66a59db764091b
fredmloth <frederiekeloth@gmail.com>
2025-09-20 16:25:46 +0200

    started working on evotorch implementation

---
+++ Fred_evotorch_test.py
@@ -0,0 +1,33 @@
+import numpy as np
+import torch
+from evotorch import Problem
+from evotorch.algorithms import CMAES
+import matplotlib.pyplot as plt
+
+import mujoco
+from mujoco import mj_step
+from mujoco import viewer
+from ariel.simulation.environments.simple_flat_world import SimpleFlatWorld
+from ariel.body_phenotypes.robogen_lite.prebuilt_robots.gecko import gecko
+
+
+# Global parameters
+
+
+# Ideas/TO DO
+# 1. Change NN -> Output structure for downstream useage the same
+
+
+# Use this function for main functions (NOT RUNNING evaluations)
+def main():
+    return
+
+
+# Use this function to run evaluations
+def run():
+    return
+
+
+if __name__ == "__main__":
+    # Test several times
+    main()
